eaff857c2941dfe58f56deae3f0bd67f21319d12
Manrique <moporto@amazon.com>
2018-03-17 14:32:36 -0700

    Making the connection unique per service

--- split-test-service/src/main/scala/com/mmanrique/distributed/experiments/cassandra/CassandraGateway.scala
+++ split-test-service/src/main/scala/com/mmanrique/distributed/experiments/cassandra/CassandraGateway.scala
@@ -1,21 +1,27 @@
 package com.mmanrique.distributed.experiments.cassandra
 
 import com.datastax.driver.core.{Cluster, Session}
+import com.typesafe.scalalogging.LazyLogging
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 import scala.beans.BeanProperty
 
 @Component
-class CassandraGateway(@Value("${cassandra.location}") @BeanProperty defaultLocation: String) {
+class CassandraGateway(@Value("${cassandra.location}") @BeanProperty defaultLocation: String,
+                       @Value("${cassandra.keyspace-name}") @BeanProperty keySpaceName: String)
+  extends LazyLogging {
+
+  lazy val cluster: Cluster = getCluster
 
 
-  def getSession(): Session = {
-    getCluster().connect("dev")
+  def getSession: Session = {
+    cluster.connect(keySpaceName)
   }
 
-  def getCluster(address: String = defaultLocation): Cluster = {
-    Cluster.builder().addContactPoint(address).build()
+  def getCluster: Cluster = {
+    logger.info("Created Cluster connection")
+    Cluster.builder().addContactPoint(defaultLocation).build()
   }
 
 }
