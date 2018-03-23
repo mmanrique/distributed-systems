0d3cf8772c43974948024905548eda34777d4262
Manrique <moporto@amazon.com>
2018-03-22 22:41:20 -0700

    split-test compiles now

--- split-service-client/src/main/scala/com/mmanrique/distributed/experiments/interface/GetSplitTestResponse.scala
+++
@@ -1,7 +0,0 @@
-package com.mmanrique.distributed.experiments.interface
-
-import com.fasterxml.jackson.annotation.JsonProperty
-
-case class GetSplitTestResponse(@JsonProperty("value") value: String) {
-
-}
